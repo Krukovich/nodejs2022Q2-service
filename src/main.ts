e81083243e33ef7da3786d4baf6d770feae2a051
Alexey Krukovich <aliaksei.krukovich@itechart-group.com>
2022-07-12 10:41:33 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const rootDirname = process.cwd();
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
